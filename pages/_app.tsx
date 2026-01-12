c2f83169fe8002115fbf22f501127b87a6532d5c
MauriceHPS <maurice@headpoint.info>
2026-01-12 14:34:30 +0300

    Renamed layouts folder to 'layout'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/layouts/Layout";
+import Layout from "@/components/layout/Layout";
 import { AppProps } from "next/app";
 import React from "react";
 import "@/styles/globals.css";
